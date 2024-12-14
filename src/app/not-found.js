6a28a95c20236a8f09a233d89f560e226b56a22f
ajayrobin1 <ajayrobin10@gmail.com>
2024-12-14 23:57:10 +0530

    apo

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -12,7 +12,7 @@ export default function Custom404() {
                 </svg>
             </p>
             <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">Page not found</h1>
-            <p className="mt-4 text-gray-500 dark:text-gray-400">The page you are looking for doesn't exist. Here are some helpful links:</p>
+            <p className="mt-4 text-gray-500 dark:text-gray-400">The page you are looking for doesn&apos;t exist. Here are some helpful links:</p>
 
             <div className="flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto">
                 <BackButton />

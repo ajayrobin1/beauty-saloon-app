c034e53e6b006628c74b513e5685c9e0007b0fd5
ajayrobin1 <ajayrobin10@gmail.com>
2024-12-17 08:04:36 +0530

    menu color and margin

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -7,7 +7,8 @@ export default function Card({imgSrc, title, id}) {
   const SvgIcon =  dynamic(() => import(`@/app/icons${imgSrc}`))
 
     return ( 
-  <div key={id} className="w-full text-left hover:bg-gray-100 lg:basis-1/2 border border-gray-400 bg-gray-50 mx-auto rounded-sm">
+      <div className="flex flex-col w-full lg:basis-1/2 mx-auto border border-gray-400 bg-gray-50  rounded-sm p-2">
+  <div key={id} className="text-left hover:bg-gray-100 ">
     <div className="relative flex">
       <SvgIcon 
         style={{width:80,height:80}} className={"p-1"}
@@ -17,13 +18,9 @@ export default function Card({imgSrc, title, id}) {
       <p className="cursor-default py-auto w-100 tracking-wide text-md text-gray-800 capitalize dark:text-gray-800">
         {title}
       </p>
-      <button className="p-2 text-sm font-semibold rounded-full select-none text-gray-800 uppercase transition-colors duration-300 transform bg-none hover:bg-green-700 dark:hover:bg-gray-800 dark:hover:text-white focus:text-green-600 focus:outline-none">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-        </svg>
-      </button>
       </div>
     </div>
   </div>
+      </div>
     );
   }
--- src/app/services/[slug]/page.js
+++ src/app/services/[slug]/page.js
@@ -17,13 +17,20 @@ const services=[
     },
     { 
       id:'3',
-      title:'Manicure and pedicure',
+      title:'Manicure',
       imgSrc: 'https://images.pexels.com/photos/3997391/pexels-photo-3997391.jpeg?q=80&w=1440&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       price:'200',
       description:"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. ",
     },
     { 
       id:'4',
+      title:'Pedicure',
+      imgSrc: 'https://images.pexels.com/photos/3997391/pexels-photo-3997391.jpeg?q=80&w=1440&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      price:'200',
+      description:"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. ",
+    },
+    { 
+      id:'5',
       title:'Hairstyle',
       imgSrc: 'https://images.pexels.com/photos/6628700/pexels-photo-6628700.jpeg?q=80&w=1440&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       price:'200',
@@ -35,10 +42,10 @@ const services=[
 export default async function Page({ params }) {
     const { slug } = await params;
     const title = decodeURIComponent(slug);
-    const file = services.find((item) => item.title.toLocaleLowerCase() === title)
+    const file = services.find((item) => item.title.toLocaleLowerCase() === title.toLocaleLowerCase())
   return(
       <>
-        <Hero imgSrc={file.imgSrc}>
+        <Hero imgSrc={file?.imgSrc}>
             <h2 className="capitalize">{file.title}</h2>
         </Hero>  
         <div className="container mx-auto md:w-1/2 p-6 py-8">

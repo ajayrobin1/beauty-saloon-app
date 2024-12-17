c9315c70971fc0d8aad789628d11626edf725011
ajayrobin1 <ajayrobin10@gmail.com>
2024-12-17 11:33:46 +0530

    changed menu

---
+++ src/app/cart/page.js
@@ -0,0 +1,51 @@
+"use client";
+import ContactForm from "@/app/components/ContactForm";
+import Hero from "@/app/components/Hero";
+
+export default function Page() {
+    const cart = [];
+    return ( 
+      <>
+      <Hero imgSrc={'https://images.unsplash.com/photo-1487412912498-0447578fcca8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}>
+        <h2 className="imperial-script-regular text-white ">
+          Your Cart
+        </h2>
+      </Hero>
+
+    <div className="bg-white p-6">
+        {
+          cart.length === 0?
+
+            <div className="mx-auto flex w-full border border-gray-400 max-w-sm overflow-hidden bg-white rounded-sm dark:bg-gray-white">
+    <div className="flex items-center justify-center w-12 bg-yellow-400">
+        <svg className="w-6 h-6 text-white fill-current" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+            <path d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM21.6667 28.3333H18.3334V25H21.6667V28.3333ZM21.6667 21.6666H18.3334V11.6666H21.6667V21.6666Z" />
+        </svg>
+    </div>
+
+    <div className="px-4 py-2 -mx-3">
+        <div className="mx-3">
+            <span className="font-semibold text-yellow-400 dark:text-yellow-400">Warning</span>
+            <p className="text-sm text-gray-600 dark:text-gray-600">
+            Your cart is empty!
+            </p>
+        </div>
+    </div>
+</div>
+          
+          :
+            <div className="flex flex-col md:flex-row justify-center p-4 w-full md:w-5/6 md:mx-auto gap-y-6 md:gap-x-2 mb-4">
+            {cart.map((item, i) => {
+                return(
+                    <Card imgSrc={item.iconPath} title={item.title} key={i} id={i} />
+                    )
+                })
+            }
+            </div>
+        }
+            </div>
+      </>
+  
+    );
+  }
+  
--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -58,22 +58,27 @@ export default function Navbar() {
         </div>
 
         {/* <!-- Mobile Menu open: "block", Menu closed: "hidden" --> */}
-        <div className={`absolute right-0 z-10 w-1/2 p-3 px-6 mt-2 transition-all duration-300 ease-in-out dark:bg-${open?'black':'none'} md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center ${open?'opacity-100 -translate-x-none': 'opacity-0 translate-x-full'}`}>
-            <div className="flex flex-col md:flex-row md:mx-6">
-                <Link  className={`my-2 transition-colors duration-300 transform ${pathname=='/'?'border-b-2 border-pink-900':'border-none'} text-gray-50 md:mx-4 md:my-0 uppercase`} href="/">Home</Link>
-                <Link className={`my-2 transition-colors duration-300 transform ${pathname=='/booking'?'border-b-2 border-pink-900':'border-none'} text-gray-50 md:mx-4 md:my-0 uppercase`} href="/booking">Booking</Link>
-                <Link className={`my-2 transition-colors duration-300 transform ${pathname=='/services'?'border-b-2 border-pink-900':'border-none'} text-gray-50 md:mx-4 md:my-0 uppercase`} href="/services">Services</Link>
-                <Link className={`my-2 transition-colors duration-300 transform ${pathname=='/gallery'?'border-b-2 border-pink-900':'border-none'} text-gray-50 md:mx-4 md:my-0 uppercase`} href="/gallery">Gallery</Link>
-                <Link className={`my-2 transition-colors duration-300 transform ${pathname=='/contact'?'border-b-2 border-pink-900':'border-none'} text-gray-50 md:mx-4 md:my-0 uppercase`} href="/contact">Contact</Link>
-            </div>
-
-            <div class="flex justify-start md:block my-2 md:mt-0">
-                <a class="relative text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-300" href="/cart">
-                    <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                    <span class="absolute top-0 left-0 p-1 text-xs text-white bg-pink-600 rounded-full"></span>
-                </a>
+        <div className={`absolute flex flex-row right-0 w-full mt-2 transition-all duration-300 ease-in-out md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center ${open?'opacity-100 -translate-x-none': 'opacity-0 translate-x-full'}`}>
+            
+            <button className={`md:hidden bg-gray-900/50 block w-1/2 h-screen backdrop-blur-sm md:-translate-x-0 ${open?'opacity-100 translate-x-none': 'opacity-0 -translate-x-full'}`} onClick={toggleMenu}>
+            <span className='hidden'>Close</span>
+            </button>
+            <div className={`ml-auto md:w-full w-1/2 dark:bg-${open?'black':'none'} md:bg-none h-screen md:h-auto`}>
+                <div className={`z-10 flex flex-col md:flex-row p-3 px-6 md:p-0  md:mx-6`}>
+                    <Link  className={`my-2 transition-colors duration-300 transform ${pathname=='/'?'border-b-2 border-pink-900':'border-none'} text-gray-50 md:mx-4 md:my-0 uppercase`} href="/">Home</Link>
+                    <Link className={`my-2 transition-colors duration-300 transform ${pathname=='/booking'?'border-b-2 border-pink-900':'border-none'} text-gray-50 md:mx-4 md:my-0 uppercase`} href="/booking">Booking</Link>
+                    <Link className={`my-2 transition-colors duration-300 transform ${pathname=='/services'?'border-b-2 border-pink-900':'border-none'} text-gray-50 md:mx-4 md:my-0 uppercase`} href="/services">Services</Link>
+                    <Link className={`my-2 transition-colors duration-300 transform ${pathname=='/gallery'?'border-b-2 border-pink-900':'border-none'} text-gray-50 md:mx-4 md:my-0 uppercase`} href="/gallery">Gallery</Link>
+                    <Link className={`my-2 transition-colors duration-300 transform ${pathname=='/contact'?'border-b-2 border-pink-900':'border-none'} text-gray-50 md:mx-4 md:my-0 uppercase`} href="/contact">Contact</Link>
+                <div className={`flex justify-start md:block mt-4 md:mt-0 px-0 md:ml-8`}>
+                    <a class="relative text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-300" href="/cart">
+                        <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        <span class="absolute top-0 left-0 p-1 text-xs text-white bg-pink-600 rounded-full"></span>
+                    </a>
+                </div>
+                </div>
             </div>
         </div>
     </div>
